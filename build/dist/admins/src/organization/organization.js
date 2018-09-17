22a1cfb6a78fb9e5e10fe9a549cf96d9e489c17b
Henry <liaohengyi@outlook.com>
2018-09-16 22:56:08 -0400

    release

--- build/dist/admins/src/organization/organization.js
+++
@@ -1,8 +0,0 @@
-import { CrApp } from "tonva-react-usql";
-//import { Page, meInFrame } from "tonva-tools";
-export class CrOrganization extends CrApp {
-    constructor() {
-        super('$$$/$unitx', undefined);
-    }
-}
-//# sourceMappingURL=organization.js.map
